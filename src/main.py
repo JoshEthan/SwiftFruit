078ed5b0002de6c6ce323f7af4b0b58382d07c22
JoshEthan <joshuaeris@gmail.com>
2022-02-06 18:23:14 -0700

    Main is fleshed out and has needed comments.

--- src/main.py
+++ src/main.py
@@ -4,11 +4,22 @@ from Dates import Dates
 from TestAccount import TestAccount
 from TestStrategy import TestStrategy
 
+"""
+The data on this page is the main data I need to change. 
+I need to add in the 'interval', 'dates', the 'strategy', and the 'account'
+"""
+
 interval = Interval._1MIN.value
-dates = Dates("2020-01-01 00:00:00", "2020-01-01 11:59:59")
+dates = Dates("2020-01-01 00:00:00", "2020-01-01 01:00:00")
 
+print("Making Account...")
+account = TestAccount(interval, dates, 100, 0)
+
+print("Making Strategy...")
 strategy = TestStrategy()
-# account = TestAccount(100, 0)
+# TODO: Make account a parameter for strategy. This makes it auto integrated into it
 
-# swiftFruit = SwiftFruit('1m', dates, strategy, account)
-# swiftFruit.run()
+print("Making SwiftFruit...")
+swiftFruit = SwiftFruit(strategy, account)
+print("Running SwiftFruit...")
+swiftFruit.run()
